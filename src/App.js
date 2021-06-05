1744bf4b73066a78badece5efc7367034a1ac21a
Keerthikumar Muralidharan <krijai@Keerthikumars-MacBook-Air.local>
2021-06-05 01:46:20 -0400

    tabs icons updated

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
 
   return(
     <div>
-      <h1>My Todo List</h1>
+      <h1 className="title">My Todo List</h1>
       <div className="main-content">
         <AddButton addNewItem={addNewItem}/>
         <Tabs>
